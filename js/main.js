9f6f5abbd37e2a77e0db9d8a75f413e33a94abd6
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2021-12-29 14:10:08 +0100

    update new changes

--- js/main.js
+++ js/main.js
@@ -9,24 +9,34 @@ const getData = () => {
     .then((response) => response.json())
     .then((paleta) => {
       data = paleta.palettes;
-      console.log(data);
       paintPaleta(data);
     });
 };
 
 //Pinto mis paletas
 const paintPaleta = (paletes) => {
-  let html = "";
+  const divContainer = document.querySelector(".js-container");
+  divContainer.textContent = "";
   for (const palete of paletes) {
-    html += `<div class="js-section-text">
-    <h3 class="texto">${palete.name} from ${palete.from}</h3>
-    <input type="checkbox" class="js-mark mark" data-id ="${palete.id}"></div>`;
+    //create name palette
+    const newTitle = document.createElement("h3");
+    newTitle.className = "text";
+    newTitle.textContent = `${palete.name} from ${palete.from}`;
+    //create palette container
+    const newContainer = document.createElement("ul");
+    newContainer.className = "list-container";
+    newContainer.id = palete.id;
     for (const color of palete.colors) {
-      html += `<div class="color__item js-div-btn" style="background-color:#${color}" data-id ="${palete.id}"></div>`;
+      //palette colors
+      const newListColor = document.createElement("li");
+      newListColor.className = "color__item";
+      newListColor.style.backgroundColor = `#${color}`;
+      newContainer.appendChild(newListColor);
     }
+    divContainer.appendChild(newTitle);
+    divContainer.appendChild(newContainer);
+    //document.querySelector(".").innerHTML = html;
   }
-  document.querySelector(".js-container").innerHTML = html;
-  setInLocalStorge();
   eventListener();
   eventSearch();
 };
@@ -34,24 +44,49 @@ const paintPaleta = (paletes) => {
 //handler
 
 const handlerFavorite = (eve) => {
-  const paleteId = eve.target.dataset.id;
+  //creo una const que me va a guardar mi id que esta en el ul.
+  const paleteId = eve.currentTarget.id;
+  const currentTarget = eve.currentTarget;
   //buscar en favorite para encontrar si esta la paleta
   let foundId = prefers.find((item) => item.id === paleteId);
+  //como prefers esta vacia va a dar undefine y entra en el if.
   if (foundId === undefined) {
-    //busco en data para econtrar la paleta
+    //busco en data para encontrar la paleta
     let foundOtherId = data.find((item) => item.id === paleteId);
+    console.log("entra al primer if");
     //si no esta la agrego
     prefers.push({
       id: foundOtherId.id,
       name: foundOtherId.name,
       colors: foundOtherId.colors,
     });
+    console.log(prefers);
+    //evaluando agregar o no a favoritos
+    currentTarget.firstChild.classList.add("fas");
+    currentTarget.firstChild.classList.add("fa-heart");
+    setInLocalStorge();
     //y si esta no hago nada
-  } else {
-    foundId;
+  } else if(foundId !== undefined){
+    console.log("entra al else");
+    currentTarget.firstChild.classList.remove("fas");
+    currentTarget.firstChild.classList.remove("fa-heart");
+    const prefersFound = prefers.find((item) => item.id === paleteId);
+    console.log(prefersFound);
+    prefers.splice(prefersFound,1);
+    
+    setInLocalStorge();
   }
+  paintFavIcon();
 };
 
+//paint favorite
+const paintFavIcon = () => {
+  const divContainer = document.querySelector(".js-container");
+  const containerUl = document.querySelector(".list-container");
+  //containerUl.firstChild.className = ".color__item fas fa-heart";
+};
+
+//busqueda del valor del input en data
 function handlerSearch() {
   //obtengo el valor del input
   const inputElement = document.querySelector(".js-input");
@@ -61,14 +96,13 @@ function handlerSearch() {
   const fillValue = data.filter((item) =>
     item.name.toLowerCase().includes(userSearch)
   );
-
   paintPaleta(fillValue);
   eventListener();
 }
 
 //listener palette
 const eventListener = () => {
-  const elementBtn = document.querySelectorAll(".js-mark");
+  const elementBtn = document.querySelectorAll(".list-container");
   for (const item of elementBtn) {
     item.addEventListener("click", handlerFavorite);
   }
@@ -87,14 +121,14 @@ const setInLocalStorge = () => {
   localStorage.setItem("data", stringifyData);
 };
 
-//
+// lo recupero
 const getFromLocalStorage = () => {
   const localStorageData = localStorage.getItem("data");
   if (localStorageData !== null) {
-    cart = JSON.parse(localStorageData);
-    paintPaleta();
+    data = JSON.parse(localStorageData);
   }
 };
 
 getData();
-paintPaleta(data);
+getFromLocalStorage();
+//paintPaleta(data);
